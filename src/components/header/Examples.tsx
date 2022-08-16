18e5ff5b50f4338264ebc51f415265aad9d35735
Joseph Li <ljx930520@gmail.com>
2022-08-16 14:02:16 +1000

    fix: fix examples menu list light mode compatibility
    fix the background color of examples menu list when it is in light mode

--- src/components/header/Examples.tsx
+++ src/components/header/Examples.tsx
@@ -1,90 +1,93 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import {ChevronDownIcon} from "@chakra-ui/icons";
 import {
-  Button,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Tooltip,
-  useToast,
+    DarkMode,
+    Button,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Tooltip,
+    useToast,
 } from "@chakra-ui/react";
-import { useWallet } from "@solana/wallet-adapter-react";
+import {useWallet} from "@solana/wallet-adapter-react";
 import React from "react";
 import {
-  useSessionStoreWithoutUndo,
-  useSessionStoreWithUndo,
+    useSessionStoreWithoutUndo,
+    useSessionStoreWithUndo,
 } from "../../hooks/useSessionStore";
-import { mapITransactionExtToITransaction } from "../../mappers/external-to-internal";
-import { EXAMPLES } from "../../utils/examples";
-import { DEFAULT_TRANSACTION_RUN } from "../../utils/state";
+import {mapITransactionExtToITransaction} from "../../mappers/external-to-internal";
+import {EXAMPLES} from "../../utils/examples";
+import {DEFAULT_TRANSACTION_RUN} from "../../utils/state";
 
 export const Example: React.FC = () => {
-  const { publicKey: walletPublicKey } = useWallet();
-  const setTransaction = useSessionStoreWithUndo((state) => state.set);
-  const setResults = useSessionStoreWithoutUndo((state) => state.set);
+    const {publicKey: walletPublicKey} = useWallet();
+    const setTransaction = useSessionStoreWithUndo((state) => state.set);
+    const setResults = useSessionStoreWithoutUndo((state) => state.set);
 
-  const toast = useToast();
+    const toast = useToast();
 
-  const loadExample = (name: string) => {
-    const { transaction, help } = EXAMPLES[name];
-    setTransaction((state) => {
-      state.transaction = mapITransactionExtToITransaction(
-        transaction(walletPublicKey?.toBase58()!)
-      );
-    });
-    setResults((state) => {
-      state.transactionRun = DEFAULT_TRANSACTION_RUN;
-    });
+    const loadExample = (name: string) => {
+        const {transaction, help} = EXAMPLES[name];
+        setTransaction((state) => {
+            state.transaction = mapITransactionExtToITransaction(
+                transaction(walletPublicKey?.toBase58()!)
+            );
+        });
+        setResults((state) => {
+            state.transactionRun = DEFAULT_TRANSACTION_RUN;
+        });
 
-    toast({
-      title: "The example has been loaded!",
-      description: help,
-      status: "info",
-      isClosable: true,
-      duration: 30_000,
-    });
-  };
+        toast({
+            title: "The example has been loaded!",
+            description: help,
+            status: "info",
+            isClosable: true,
+            duration: 30_000,
+        });
+    };
 
-  return (
-    <Menu>
-      <Tooltip
-        shouldWrapChildren
-        hasArrow={!walletPublicKey}
-        label={
-          walletPublicKey
-            ? "Load an example transaction"
-            : "Please connect a wallet to contiune"
-        }
-      >
-        <MenuButton
-          as={Button}
-          rightIcon={<ChevronDownIcon />}
-          variant="ghost"
-          textColor="white"
-          isDisabled={!walletPublicKey}
-        >
-          Examples
-        </MenuButton>
-      </Tooltip>
+    return (
+        <Menu>
+            <DarkMode>
+                <Tooltip
+                    shouldWrapChildren
+                    hasArrow={!walletPublicKey}
+                    label={
+                        walletPublicKey
+                            ? "Load an example transaction"
+                            : "Please connect a wallet to contiune"
+                    }
+                >
+                    <MenuButton
+                        as={Button}
+                        rightIcon={<ChevronDownIcon/>}
+                        variant="ghost"
+                        textColor="white"
+                        isDisabled={!walletPublicKey}
+                    >
+                        Examples
+                    </MenuButton>
+                </Tooltip>
+            </DarkMode>
 
-      <MenuList fontSize="md" zIndex="modal">
-        <MenuItem
-          onClick={() => {
-            loadExample("systemProgramTransfer");
-          }}
-        >
-          System Program: Transfer SOL
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            loadExample("systemProgramCreateAccount");
-          }}
-        >
-          System Program: Create Account
-        </MenuItem>
+            <MenuList fontSize="md" zIndex="modal">
+                <MenuItem
+                    onClick={() => {
+                        loadExample("systemProgramTransfer");
+                    }}
+                >
+                    System Program: Transfer SOL
+                </MenuItem>
+                <MenuItem
+                    onClick={() => {
+                        loadExample("systemProgramCreateAccount");
+                    }}
+                >
+                    System Program: Create Account
+                </MenuItem>
 
-        {/* TODO more examples */}
-      </MenuList>
-    </Menu>
-  );
+                {/* TODO more examples */}
+            </MenuList>
+        </Menu>
+    );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,65 +1,68 @@
-import { HamburgerIcon, InfoIcon } from "@chakra-ui/icons";
+import {HamburgerIcon, InfoIcon} from "@chakra-ui/icons";
 import {
-  DarkMode,
-  Flex,
-  Hide,
-  Icon,
-  IconButton,
-  Image,
-  Spacer,
-  Text,
-  Tooltip,
-  useToast,
+    DarkMode,
+    Flex,
+    Hide,
+    Icon,
+    IconButton,
+    Image,
+    Spacer,
+    Text,
+    Tooltip,
+    useToast,
 } from "@chakra-ui/react";
-import { useState } from "react";
-import { FaWrench } from "react-icons/fa";
-import { useSessionStoreWithoutUndo } from "../../hooks/useSessionStore";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Example } from "./Examples";
-import { WalletButton } from "./WalletButton";
+import {useState} from "react";
+import {FaWrench} from "react-icons/fa";
+import {useSessionStoreWithoutUndo} from "../../hooks/useSessionStore";
+import {ColorModeSwitcher} from "./ColorModeSwitcher";
+import {Example} from "./Examples";
+import {WalletButton} from "./WalletButton";
 
 export const Header: React.FC = () => {
-  const [funTitle, setFunTitle] = useState(true);
-  const toast = useToast();
-  const set = useSessionStoreWithoutUndo((state) => state.set);
+    const [funTitle, setFunTitle] = useState(true);
+    const toast = useToast();
+    const set = useSessionStoreWithoutUndo((state) => state.set);
 
-  return (
-    <Flex p="2" bgColor="main.800" alignItems="center">
-      <DarkMode>
-        <Image w="40px" h="40px" src="/logo128.png" alt="Logo" />
+    return (
+        <Flex p="2" bgColor="main.800" alignItems="center">
+            <DarkMode>
+                <Image w="40px" h="40px" src="/logo128.png" alt="Logo"/>
+            </DarkMode>
 
-        <Hide below="md">
-          <Text
-            ml="3"
-            mr="9"
-            pr="2" // for cursive font to not get cut off
-            color="white"
-            fontFamily={funTitle ? "'Dancing Script', cursive;" : ""}
-            fontWeight="extrabold"
-            fontSize={funTitle ? "3xl" : "xl"}
-            bgGradient="linear(to-r, #ff6f61ff, #fece2f)"
-            bgClip="text"
-            onClick={() => {
-              toast({
-                title: funTitle
-                  ? "Serious Business Mode™️ enabled!"
-                  : "Serious Business Mode™️ disabled!",
-                status: funTitle ? "warning" : "success",
-                duration: 1000,
-                isClosable: true,
-              });
-              setFunTitle(!funTitle);
-            }}
-          >
-            Better Call Sol
-          </Text>
-          <Example />
-        </Hide>
+            <Hide below="md">
+                <DarkMode>
+                    <Text
+                        ml="3"
+                        mr="9"
+                        pr="2" // for cursive font to not get cut off
+                        color="white"
+                        fontFamily={funTitle ? "'Dancing Script', cursive;" : ""}
+                        fontWeight="extrabold"
+                        fontSize={funTitle ? "3xl" : "xl"}
+                        bgGradient="linear(to-r, #ff6f61ff, #fece2f)"
+                        bgClip="text"
+                        onClick={() => {
+                            toast({
+                                title: funTitle
+                                    ? "Serious Business Mode™️ enabled!"
+                                    : "Serious Business Mode™️ disabled!",
+                                status: funTitle ? "warning" : "success",
+                                duration: 1000,
+                                isClosable: true,
+                            });
+                            setFunTitle(!funTitle);
+                        }}
+                    >
+                        Better Call Sol
+                    </Text>
+                </DarkMode>
+                <Example/>
+            </Hide>
 
-        <Spacer />
+            <Spacer/>
 
-        {/* TODO implement */}
-        {/* <Hide below="md">
+            {/* TODO implement */}
+            {/* <Hide below="md">
           <Tooltip label="Undo">
             <IconButton
               mr="0.5"
@@ -81,62 +84,61 @@ export const Header: React.FC = () => {
             />
           </Tooltip>
         </Hide> */}
-      </DarkMode>
 
-      <ColorModeSwitcher justifySelf="flex-end" />
+            <ColorModeSwitcher justifySelf="flex-end"/>
 
-      <DarkMode>
-        <Tooltip label="Options">
-          <IconButton
-            mr="0.5"
-            aria-label="Options"
-            icon={<Icon as={FaWrench} />}
-            variant="ghost"
-            color="white"
-            onClick={() => {
-              set((state) => {
-                state.uiState.optionsOpen = true;
-              });
-            }}
-          />
-        </Tooltip>
+            <DarkMode>
+                <Tooltip label="Options">
+                    <IconButton
+                        mr="0.5"
+                        aria-label="Options"
+                        icon={<Icon as={FaWrench}/>}
+                        variant="ghost"
+                        color="white"
+                        onClick={() => {
+                            set((state) => {
+                                state.uiState.optionsOpen = true;
+                            });
+                        }}
+                    />
+                </Tooltip>
 
-        <Tooltip label="Info">
-          <IconButton
-            mr="1"
-            aria-label="Info"
-            icon={<InfoIcon />}
-            variant="ghost"
-            color="white"
-            onClick={() => {
-              set((state) => {
-                state.uiState.infoOpen = true;
-              });
-            }}
-          />
-        </Tooltip>
-      </DarkMode>
+                <Tooltip label="Info">
+                    <IconButton
+                        mr="1"
+                        aria-label="Info"
+                        icon={<InfoIcon/>}
+                        variant="ghost"
+                        color="white"
+                        onClick={() => {
+                            set((state) => {
+                                state.uiState.infoOpen = true;
+                            });
+                        }}
+                    />
+                </Tooltip>
+            </DarkMode>
 
-      <WalletButton />
+            <WalletButton/>
 
-      <DarkMode>
-        <Hide below="md">
-          <Tooltip label="Palette">
-            <IconButton
-              mr="0.5"
-              aria-label="Palette"
-              icon={<HamburgerIcon />}
-              variant="ghost"
-              color="white"
-              onClick={() => {
-                set((state) => {
-                  state.uiState.paletteOpen = !state.uiState.paletteOpen;
-                });
-              }}
-            />
-          </Tooltip>
-        </Hide>
-      </DarkMode>
-    </Flex>
-  );
+            <DarkMode>
+                <Hide below="md">
+                    <Tooltip label="Palette">
+                        <IconButton
+                            mr="0.5"
+                            aria-label="Palette"
+                            icon={<HamburgerIcon/>}
+                            variant="ghost"
+                            color="white"
+                            onClick={() => {
+                                set((state) => {
+                                    state.uiState.paletteOpen = !state.uiState.paletteOpen;
+                                });
+                            }}
+                        />
+                    </Tooltip>
+                </Hide>
+            </DarkMode>
+        </Flex>
+    );
 };
