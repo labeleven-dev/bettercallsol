00dc7184d3e4265e3091a801339a8ae95ca1795f
sohrab <4444588+sohrab-@users.noreply.github.com>
2022-08-24 07:17:20 +1000

    fix: use radio buttons for encoding

--- src/components/client/data/RawData.tsx
+++ src/components/client/data/RawData.tsx
@@ -1,9 +1,16 @@
-import { Box, Icon, Textarea, useToast } from "@chakra-ui/react";
+import {
+  Flex,
+  Grid,
+  Radio,
+  RadioGroup,
+  Spacer,
+  Stack,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
 import bs58 from "bs58";
 import React from "react";
-import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 import { useInstruction } from "../../../hooks/useInstruction";
-import { ToggleIconButton } from "../../common/ToggleIconButton";
 
 export const RawData: React.FC = () => {
   const { useShallowGet, update } = useInstruction();
@@ -14,33 +21,52 @@ export const RawData: React.FC = () => {
 
   const toast = useToast();
 
-  const onToggle = () => {
-    update((state) => {
-      if (encoding === "hex") {
+  const onChange = (encoding: string) => {
+    if (encoding === "hex") {
+      update((state) => {
         state.data.raw.content = bs58.encode(Buffer.from(content, "hex"));
-        state.data.raw.encoding = "bs58";
-      } else {
-        try {
-          state.data.raw.content = Buffer.from(
-            bs58.decode(state.data.raw.content)
-          ).toString("hex");
-          state.data.raw.encoding = "hex";
-        } catch (e) {
-          // it is not a valid base58 string
-          toast({
-            title: "Invalid string",
-            description: "This is not a valid base58 encoded string.",
-            status: "error",
-            isClosable: true,
-            duration: 30_000,
-          });
-        }
+        state.data.raw.encoding = "hex";
+      });
+    }
+
+    if (encoding === "bs58") {
+      try {
+        const encoded = Buffer.from(bs58.decode(content)).toString("hex");
+        update((state) => {
+          state.data.raw.content = encoded;
+          state.data.raw.encoding = "bs58";
+        });
+      } catch (e) {
+        // it is not a valid base58 string
+        toast({
+          title: "Invalid string",
+          description: "This is not a valid base58 encoded string.",
+          status: "error",
+          isClosable: true,
+          duration: 30_000,
+        });
       }
-    });
+    }
   };
 
   return (
-    <Box display="flex">
+    <Grid>
+      <Flex>
+        <Spacer />
+        <RadioGroup
+          size="sm"
+          mb="1"
+          mr="2"
+          value={encoding}
+          onChange={onChange}
+        >
+          <Stack direction="row">
+            <Radio value="bs58">Base 58</Radio>
+            <Radio value="hex">Hex</Radio>
+          </Stack>
+        </RadioGroup>
+      </Flex>
+
       <Textarea
         flex="1"
         fontFamily="mono"
@@ -56,21 +82,6 @@ export const RawData: React.FC = () => {
           });
         }}
       />
-
-      <Box ml="auto" mt="auto">
-        <ToggleIconButton
-          ml="1"
-          size="sm"
-          label={
-            encoding === "hex"
-              ? "Click to use encoded data"
-              : "Click to use hex data"
-          }
-          icon={<Icon as={HiOutlineSwitchHorizontal} />}
-          toggled={encoding !== "hex"}
-          onToggle={onToggle}
-        />
-      </Box>
-    </Box>
+    </Grid>
   );
 };
