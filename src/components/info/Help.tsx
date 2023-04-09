048c027271aafde4efefabfcb9870ddf78837c60
sohrab <4444588+sohrab-@users.noreply.github.com>
2023-04-09 13:17:03 +1000

    feat: provide more info in the help box

--- src/components/info/Help.tsx
+++ src/components/info/Help.tsx
@@ -1,7 +1,49 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Link as CharkaLink,
+  Grid,
+  LinkProps,
+  ListItem,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
+
+const Link: React.FC<LinkProps> = ({ children, ...theRest }: LinkProps) => (
+  <CharkaLink
+    bgGradient="linear(45deg,#9945ff,#19fb9d)"
+    bgClip="text"
+    isExternal
+    _hover={{ textShadow: "2px 2px 5px #9945FF" }}
+    {...theRest}
+  >
+    {children}
+  </CharkaLink>
+);
 
 export const Help: React.FC = () => (
-  <Text pt="10" pb="10" textAlign="center">
-    Docs are coming soon!
-  </Text>
+  <Grid gridGap="3" my="5">
+    <Text>Docs are coming soon!</Text>
+    <Text>In the meanwhile, you can:</Text>
+    <UnorderedList spacing="2">
+      <ListItem>
+        Load an example from Examples menu, to get a guided walkthrough of how
+        to use this tool,
+      </ListItem>
+      <ListItem>
+        Watch the{" "}
+        <Link href="https://www.youtube.com/watch?v=z4TzkuKLWAA">
+          Solana Transactions with Better Call Sol
+        </Link>{" "}
+        video to see the tool in action, or
+      </ListItem>
+      <ListItem>
+        Ask questions on the{" "}
+        <Link href="https://github.com/labeleven-dev/bettercallsol/discussions">
+          Discusson Board
+        </Link>
+        .
+      </ListItem>
+    </UnorderedList>
+    <Text></Text>
+    <Text></Text>
+  </Grid>
 );
